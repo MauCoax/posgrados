2687b94bedcacdd17867f36ef94c5d064a51eb8b
MauCoax <maud3ca@hotmail.es>
2018-06-12 16:58:13 -0600

    BASE_DIR agruegado

--- posgrados/settings.py
+++ posgrados/settings.py
@@ -120,4 +120,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE= 'whitenoise.django.GzipManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join((BASE.DIR),'static')
+STATIC_ROOT = os.path.join((BASE_DIR),'static')
