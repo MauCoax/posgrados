e6a26acdbb88b796366985bfd7a20f3088b99172
MauCoax <maud3ca@hotmail.es>
2018-06-12 16:54:14 -0600

    statics roots agregadas

--- posgrados/settings.py
+++ posgrados/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8w=$(8g76!t8=^mctv_kuqzx^$mh-4c01cxx0%*sryn3wja12i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['postgrados.herokuapp.com']
 
 
 # Application definition
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE= 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join((BASE.DIR),'static')
