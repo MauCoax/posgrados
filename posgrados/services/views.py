7928a43517c0b35008a985ff4adde1e0e74d481f
MauCoax <maud3ca@hotmail.es>
2018-06-12 15:44:04 -0600

    primer commit

---
+++ posgrados/services/views.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from posgrados.services.serializers import UserSerializer, GroupSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
---
+++ services/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url, include
+from rest_framework import routers
+from posgrados.services import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
