bd97883cca9f6b9787f3d0bc509dd6d44bf04f48
MauCoax <maud3ca@hotmail.es>
2018-06-13 14:21:00 -0600

    modificacion url usuarios

--- posgrados/services/urls.py
+++ posgrados/services/urls.py
@@ -1,14 +1,18 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from posgrados.services import views
+from services.views import *
+
+from . import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
+router.register(r'usuario',views.)
 
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^usuarios/$', crear_usuario, name='crear_usuario'),
 ]
