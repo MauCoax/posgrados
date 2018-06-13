74d5d0d1cb59bb8c3bed4ca5d14a0cba18cb1ffc
MauCoax <maud3ca@hotmail.es>
2018-06-13 13:13:54 -0600

    correcion urls

--- posgrados/services/urls.py
+++ posgrados/services/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from . import views
+from posgrados.services import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
