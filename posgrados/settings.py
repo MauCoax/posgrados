b1802a13bb7955971576a4fe9c0b4b65472f5a94
MauCoax <maud3ca@hotmail.es>
2018-06-13 13:27:51 -0600

    instalacion de services2

--- posgrados/settings.py
+++ posgrados/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'services.apps.ServicesConfig',
+    'services',
 ]
 
 MIDDLEWARE = [
