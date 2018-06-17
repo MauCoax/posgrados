7996f0693b01af83d3d5eecdaa12c5386e33e4eb
MauCoax <maud3ca@hotmail.es>
2018-06-16 22:22:44 -0600

    url viewsets7

--- posgrados/services/urls.py
+++ posgrados/services/urls.py
@@ -14,7 +14,7 @@ router.register(r'usuarios', views.UsuarioViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = router.urls
-    
+
     #[
     #url(r'^', include(router.urls)),
     #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
