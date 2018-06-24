72edb1bdd072c882750f7f864dd86236325f8b3c
MauCoax <maud3ca@hotmail.es>
2018-06-23 21:13:52 -0600

    corsheaders

--- posgrados/settings.py
+++ posgrados/settings.py
@@ -39,9 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'posgrados.services',
+    'corsheaders',
+
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
